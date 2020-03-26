a38c7022ad30da095079d8979d61b7a71dfd7388
Caleb Shade <pokesm4shvids@gmail.com>
2020-03-26 19:32:56 -0400

    Added a connection to the PostgreSQL database and authentication.

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-
+const { Client } = require('pg');
 var app = express();
-
+const client = new Client()
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+client.connect();
 
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  console.log(req.headers);
-  res.send(req.body);
+
+app.post('/login', (request, response) => {
+  const password = request.body.password
+  const email = request.body.email
+  const emailvalues = [request.body.email]
+  client.query('SELECT * FROM users WHERE email=$1', emailvalues, (err, res) => {
+    if(err) {
+      console.log(err)
+    } else {
+      if (res.rows[0].email == email && res.rows[0].password == password) {
+        console.log("Logged in as " + res.rows[0].name + " from " + res.rows[0].team + ".")
+      } else {
+        console.log("Email/Password is incorrect.")
+      }
+    }
+  })
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +50,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
